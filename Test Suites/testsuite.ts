ddf6a2084a351f14b0cac4207d644c0f517b0779
John Smith <hieulaai552@gmail.com>
2020-09-27 05:09:27 +0700

    44.01.104.089

---
+++ Test Suites/testsuite.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>testsuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d677c0fb-5540-4c71-a927-bf13860c1461</testSuiteGuid>
+   <testCaseLink>
+      <guid>14cbf431-f4d6-4eb0-b42a-e4cb346a2b98</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test1-login with no username no pw</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b1432b77-14f6-4e6c-8bed-ddcc6d6d4cf2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test2-login with username no pw</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ed688f01-d49f-4a88-9eae-1d1684da2b85</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test4-login with wrong pw</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>50ea6eae-3c8c-41ac-ab77-992d840b0c37</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test3-login with wrong username</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7f72bdbb-3537-48cc-8917-a2e54d0ab15e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test5-login with username and pw</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
